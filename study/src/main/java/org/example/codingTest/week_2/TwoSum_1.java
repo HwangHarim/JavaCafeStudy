99d55cd0850c6aaad22a7f6bec85f3fa87518119
HwangHarim <scopan01@naver.com>
2024-05-08 16:17:10 +0900

    feature : codingTest study start week_2

--- study/src/main/java/org/example/codingTest/week_2/TwoSum_1.java
+++ study/src/main/java/org/example/codingTest/week_2/TwoSum_1.java
@@ -1,17 +1,33 @@
 package org.example.codingTest.week_2;
 
+import java.util.*;
+
 public class TwoSum_1 {
-    public int[] twoSum(int[] nums, int target) {
-        for(int start= 0; start<nums.length; start++){
-            for(int index = start+1; index<nums.length; index++){
-                int sum = nums[start] + nums[index];
+    // 4 ms
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            numsMap.put(nums[i], i);
+        }
 
-                if(sum == target){
-                    return new int[]{start, index};
-                }
-
+        for(int i =0; i<nums.length; i++){
+            if(numsMap.containsKey(target -nums[i])&& i != numsMap.get(target -nums[i])){
+                return new int[]{i, numsMap.get(target-nums[i])};
             }
         }
         return null;
     }
+
+    // 0ms
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 1; i < nums.length; i++) {
+//            for (int l = i; l < nums.length; l++) {
+//                if (nums[l] + nums[l - i] == target) {
+//                    return new int[] { l, l - i };
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
 }
